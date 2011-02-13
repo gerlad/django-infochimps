93287c638bafa2de805d12adb43d2d0f4160311c
gerlad <gmccollam@gmail.com>
2011-02-13 12:00:36 -0500

    function belongs in api lib

--- templatetags/chimps_tag.py
+++ templatetags/chimps_tag.py
@@ -2,11 +2,3 @@ from django import template
 from request.models import Request
 
 register = template.Library()
-
-def strong_links(links):
-    screen_name, user_id = (links['screen_name'], links['user_id'])
-    for idx in range(len(links['strong_links'])):
-        strong_link = links['strong_links'][idx]
-        if strong_link is None:
-            continue
-        # get screen_name for id (id is link[0], link strength is link[1])
